3e722f816413cab9c88cf465dc6e9075c6dd39c5
WorstProgrammerEver45 <tuhindiscord@gmail.com>
2021-09-21 22:27:28 +0600

    fuck this shit im out

--- src/learningjava/Main.java
+++ src/learningjava/Main.java
@@ -34,7 +34,7 @@ public class Main {
 		JFrame mainframe = new JFrame(); // creating a frame or screen of the app
 		mainframe.setTitle("TestFrame"); // title of the frane
 		mainframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // if you do not set this to exit on close it will
-																	// remain a background proccess
+		// remain a background proccess
 		mainframe.setSize(700, 700); // size(resolution) of the app/frame
 		mainframe.setVisible(true); // setting its visibility to true otherwise will not show.
 		mainframe.getContentPane().setBackground(Color.DARK_GRAY); // setting background colour of screen of app/frame
