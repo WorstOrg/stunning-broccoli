715cfb143fcad7274bd08fa2d4cb1938d8d1c279
WorstProgrammerEver45 <tuhindiscord@gmail.com>
2021-09-22 19:48:14 +0600

    lol fixed!!!

--- src/learningjava/Main.java
+++ src/learningjava/Main.java
@@ -5,6 +5,7 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.border.Border;
 import java.awt.Color;
 import java.awt.Font;
@@ -13,14 +14,14 @@ import java.util.Scanner;
 @SuppressWarnings("unused")
 
 public class Main {
-
-	// when u come back after 1 and half month and see no comments be like...
-	// read the documentation!!! + trying as best to add coments so sry.
 	public static void main(String[] args) {
 
 		ImageIcon image = new ImageIcon("cool.png"); // setting a image so that a label can use it as an image
 
 		Border border = BorderFactory.createLineBorder(Color.GREEN, 3); // a border line cuz idk
+
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.GREEN);
 
 		JLabel label = new JLabel(); // creating a label a label is text + img or only text
 		label.setText("You bored broh?"); // sets text to the string in the "these marks"
@@ -32,15 +33,20 @@ public class Main {
 		label.setIcon(image); // setting a image from a variable. (we declared this early on.)
 		label.setBorder(border); // setting a border from a variable. (we declared this early on.)
 		label.setVerticalAlignment(JLabel.CENTER); // set whole label position of vertical this is used for images.
-		label.setHorizontalAlignment(JLabel.CENTER);
+		label.setHorizontalAlignment(JLabel.CENTER); // set whole label position of horizantal this is used for images.
+		label.setBackground(Color.GREEN); // background color of label only
+		label.setBounds(0, 0, 635, 635); // bounds of label
+		label.setOpaque(true); // gotta set this shit for background color
 
-		JFrame mainframe = new JFrame();
-		mainframe.setTitle("TestFrame");
-		mainframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		mainframe.setSize(700, 700);
-		mainframe.setVisible(true);
-		mainframe.getContentPane().setBackground(Color.DARK_GRAY);
-		mainframe.add(label);
+		JFrame mainframe = new JFrame(); // creating a frame or screen of the app
+		mainframe.setTitle("TestFrame"); // title of the frane
+		mainframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // if you do not set this to exit on close it will
+		// remain a background proccess
+		mainframe.setSize(1200, 800); // size(resolution) of the app/frame
+		mainframe.setVisible(true); // setting its visibility to true otherwise will not show.
+		mainframe.getContentPane().setBackground(Color.DARK_GRAY); // setting background colour of screen of app/frame
+		mainframe.add(label); // adding a label
+		mainframe.setLayout(null); // setting the bounds/layout
 
 	}
 }
